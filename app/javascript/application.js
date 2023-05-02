79a68a626967f61133b0289b529a22379f9e99e8
admirerbrown <admirerbrown@gmail.com>
2023-05-02 15:38:01 +0000

    Add greeting component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,13 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-// import "./controllers"
-
+// import "./controllers;
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+import App from './components/App';
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root'),
+<App/>,
+document.getElementById('root'),
 );
+
+
